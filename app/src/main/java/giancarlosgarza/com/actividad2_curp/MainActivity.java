adb5b1e7c512da697529d652cc9cf85493efdeca
Giancarlos Garza <giancarlosgza@gmail.com>
2018-08-24 02:17:46 -0500

    Initial commit First commit

---
+++ app/src/main/java/giancarlosgarza/com/actividad2_curp/MainActivity.java
@@ -0,0 +1,13 @@
+package giancarlosgarza.com.actividad2_curp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
